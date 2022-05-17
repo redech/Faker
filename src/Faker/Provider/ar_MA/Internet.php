bce347e621b2661f75868d7c0b2b811cb32f885c
Redouane Chlihi <contact@chlihi.com>
2022-05-17 18:58:47 +0100

    Add files via upload

---
+++ src/Faker/Provider/ar_MA/Internet.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Faker\Provider\ar_MA;
+
+class Internet extends \Faker\Provider\Internet
+{
+    protected static $freeEmailDomain = ['gmail.com', 'yahoo.com', 'menara.ma', 'inwi.ma', 'orange.ma', 'iam.ma', 'live.com', 'hotmail.com', 'outlook.com', 'wanadoo.ma'];
+    protected static $tld = ['com', 'com', 'com', 'net', 'org', 'ma', 'ma', 'ma'];
+}
